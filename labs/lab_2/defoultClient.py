d041a18bfb4f05de863929af0cba2e25e2843b52
ChertProduction <ChertProduction@users.noreply.github.com>
2021-03-17 23:59:00 +0200

    1

--- labs/lab_2/defoultClient.py
+++ labs/lab_2/defoultClient.py
@@ -1,39 +1,3 @@
-# from socket import socket, AF_INET, SOCK_STREAM
-# import threading as t
-# import json
-#
-# HOST = '127.0.0.1'          #localhost
-# PORT = 8080                 # порт
-# CODING = "utf-8"            # кодування
-# QUEUE_LEN = (93 + 23)-90    # кількість допустимих підключень
-#
-#
-# ##################################################################################
-# #функція зчитування даних з сервера
-# #принимает
-# #sock - сокет
-# ##################################################################################
-# def readRequest(sock):
-#     while True:
-#         data = sock.recv(1024)
-#         print(json.loads(data.decode(CODING)))                     #зчитуємо формат json
-#
-# #головна функція
-# def main():
-#     USERS = []                                                     #створюємо список клієнтів
-#     for i in range(0, QUEUE_LEN):
-#         sock = socket(AF_INET, SOCK_STREAM)                        #створюємо QUEUE_LEN-у кількість
-#         sock.connect((HOST, PORT))                                 #сокетів
-#         USERS.append(sock)                                         #закидуємо їх у список клієнтів
-#
-#     while True:
-#         for user in USERS:                                        #кожному клієнту відправляємо
-#             user.send(input(">").encode(CODING))                  #запит про введення числа
-#
-# if __name__ == '__main__':
-#     main()
-
-from socket import socket, AF_INET, SOCK_STREAM
 import asyncio
 import json
 import math
