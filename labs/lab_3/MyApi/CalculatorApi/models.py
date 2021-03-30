231c9f7a84852a62579025bb15b305a677d2162e
ChertProduction <ChertProduction@users.noreply.github.com>
2021-03-31 00:00:56 +0300

    1

--- labs/lab_3/MyApi/CalculatorApi/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class CalculateRequest:
-    def __init__(self, input_value: int):
-        self.input_value = input_value
-
-class CalculateResponse:
-    def __init__(self, result: int):
-        self.output_value = result
--- labs/lab_3/MyApi/CalculatorApi/utils.py
+++
@@ -1,7 +0,0 @@
-class Calculator():
-    """
-    Клас, що імітує роботу бізнес-логіки
-    """
-    @staticmethod
-    def calculate(input_value: int):
-        return input_value + 10
